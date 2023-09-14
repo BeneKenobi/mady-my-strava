733867683066936394a9918e3db484e3cdf3f8d1
BeneKeneobi <1708125+BeneKenobi@users.noreply.github.com>
2023-09-14 09:01:24 +0200

    minor tweaks

--- src/madymystrava/main.py
+++ src/madymystrava/main.py
@@ -8,42 +8,50 @@ from dotenv import load_dotenv, set_key
 
 load_dotenv()
 
+
 def build_auth_url(client_id: str, redirect_uri: str) -> str:
     params = {
-        'client_id': client_id,
-        'redirect_uri': redirect_uri,
-        'response_type': 'code',
-        'scope': 'read,activity:read,activity:write'
+        "client_id": client_id,
+        "redirect_uri": redirect_uri,
+        "response_type": "code",
+        "scope": "read,activity:read,activity:write",
     }
     return f"https://www.strava.com/oauth/authorize?{urlencode(params)}"
 
-def get_strava_tokens(client_id: str, client_secret: str, authorization_code: str) -> Dict[str, Any]:
+
+def get_strava_tokens(
+    client_id: str, client_secret: str, authorization_code: str
+) -> Dict[str, Any]:
     payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'code': authorization_code,
-        'grant_type': 'authorization_code'
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": authorization_code,
+        "grant_type": "authorization_code",
     }
-    response = requests.post('https://www.strava.com/oauth/token', data=payload)
+    response = requests.post("https://www.strava.com/oauth/token", data=payload)
     if response.status_code == 200:
         return response.json()
     else:
         print(f"Failed to exchange code for tokens: {response.json()}")
         return {}
 
-def refresh_strava_token(client_id: str, client_secret: str, refresh_token: str) -> Dict[str, Any]:
+
+def refresh_strava_token(
+    client_id: str, client_secret: str, refresh_token: str
+) -> Dict[str, Any]:
     payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'refresh_token': refresh_token,
-        'grant_type': 'refresh_token'
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "refresh_token": refresh_token,
+        "grant_type": "refresh_token",
     }
-    response = requests.post('https://www.strava.com/oauth/token', data=payload)
+    response = requests.post("https://www.strava.com/oauth/token", data=payload)
     if response.status_code == 200:
         return response.json()
     else:
         print(f"Failed to refresh token: {response.json()}")
         return {}
+
 
 def get_yoga_activities(access_token: str, after: int) -> List[Dict[str, Any]]:
     url = "https://www.strava.com/api/v3/athlete/activities"
@@ -54,10 +62,11 @@ def get_yoga_activities(access_token: str, after: int) -> List[Dict[str, Any]]:
 
     if response.status_code == 200:
         activities = response.json()
-        return [activity for activity in activities if activity['type'] == 'Yoga']
+        return [activity for activity in activities if activity["type"] == "Yoga"]
     else:
         print(f"Error: {response.json()}")
         return []
+
 
 def update_activity_name(access_token: str, activity_id: int, new_name: str) -> None:
     url = f"https://www.strava.com/api/v3/activities/{activity_id}"
@@ -71,25 +80,32 @@ def update_activity_name(access_token: str, activity_id: int, new_name: str) -> 
     else:
         print(f"Error: {response.json()}")
 
+
 if __name__ == "__main__":
     CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
     CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
 
     if CLIENT_ID is None or CLIENT_SECRET is None:
-        print("Error: Missing STRAVA_CLIENT_ID or STRAVA_CLIENT_SECRET environment variables.")
+        print(
+            "Error: Missing STRAVA_CLIENT_ID or STRAVA_CLIENT_SECRET environment"
+            " variables."
+        )
         exit(1)
 
     REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN")
     REDIRECT_URI = "http://localhost"
 
     NEW_NAME = "#yogamitmady"
-    
+
     if not REFRESH_TOKEN:
-        print(f"Navigate to the following URL to get your authorization code: {build_auth_url(CLIENT_ID, REDIRECT_URI)}")
+        print(
+            "Navigate to the following URL to get your authorization code:"
+            f" {build_auth_url(CLIENT_ID, REDIRECT_URI)}"
+        )
         authorization_code = input("Enter the authorization code: ")
         token_data = get_strava_tokens(CLIENT_ID, CLIENT_SECRET, authorization_code)
         REFRESH_TOKEN = token_data.get("refresh_token", "")
-        if type(REFRESH_TOKEN) is not str:
+        if not isinstance(REFRESH_TOKEN, str):
             print("Error: Failed to get refresh token.")
             exit(1)
         set_key(".env", "STRAVA_REFRESH_TOKEN", REFRESH_TOKEN)
@@ -97,13 +113,13 @@ if __name__ == "__main__":
     token_data = refresh_strava_token(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
     ACCESS_TOKEN = token_data.get("access_token", "")
 
-    # Calculate the timestamp for 24 hours ago
+    # Calculate the timestamp for three days ago
     now = datetime.now()
-    yesterday = now - timedelta(days=1)
+    yesterday = now - timedelta(days=3)
     after_timestamp = int(yesterday.timestamp())
 
     yoga_activities = get_yoga_activities(ACCESS_TOKEN, after_timestamp)
 
     for activity in yoga_activities:
-        if activity['name'] != NEW_NAME:
-            update_activity_name(ACCESS_TOKEN, activity['id'], NEW_NAME)
+        if activity["name"] != NEW_NAME:
+            update_activity_name(ACCESS_TOKEN, activity["id"], NEW_NAME)
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,38 +2,29 @@ import json
 from typing import Any, Dict, List
 from unittest.mock import Mock, patch
 
-import pytest
-
-from src.madymystrava.main import get_yoga_activities, update_activity_name
+from src.madymystrava.main import (
+    build_auth_url,
+    get_yoga_activities,
+    update_activity_name,
+)
 
 # Sample response for the /athlete/activities API
-sample_activities_response: str = json.dumps([
-    {
-        "id": 1,
-        "name": "Morning Run",
-        "type": "Run"
-    },
-    {
-        "id": 2,
-        "name": "Evening Yoga",
-        "type": "Yoga"
-    },
-    {
-        "id": 3,
-        "name": "#yogamitmady",
-        "type": "Yoga"
-    }
-])
+sample_activities_response: str = json.dumps(
+    [
+        {"id": 1, "name": "Morning Run", "type": "Run"},
+        {"id": 2, "name": "Evening Yoga", "type": "Yoga"},
+        {"id": 3, "name": "#yogamitmady", "type": "Yoga"},
+    ]
+)
 
 # Sample response for the /activities/{id} API
-sample_update_response: str = json.dumps({
-    "id": 2,
-    "name": "#yogamitmady",
-    "type": "Yoga"
-})
+sample_update_response: str = json.dumps(
+    {"id": 2, "name": "#yogamitmady", "type": "Yoga"}
+)
 
-@patch('os.getenv')
-@patch('requests.get')
+
+@patch("os.getenv")
+@patch("requests.get")
 def test_get_yoga_activities(mock_get: Mock, mock_getenv: Mock) -> None:
     mock_getenv.return_value = "dummy_token"
     mock_get.return_value.status_code = 200
@@ -41,13 +32,27 @@ def test_get_yoga_activities(mock_get: Mock, mock_getenv: Mock) -> None:
 
     activities: List[Dict[str, Any]] = get_yoga_activities("dummy_token", 12345)
     assert len(activities) == 2
-    assert activities[0]['id'] == 2
-    assert activities[1]['id'] == 3
+    assert activities[0]["id"] == 2
+    assert activities[1]["id"] == 3
 
-@patch('requests.put')
+
+@patch("requests.put")
 def test_update_activity_name(mock_put: Mock) -> None:
     mock_put.return_value.status_code = 200
     mock_put.return_value.json.return_value = json.loads(sample_update_response)
 
     update_activity_name("dummy_token", 2, "#yogamitmady")
     mock_put.assert_called_once()
+
+
+def test_build_auth_url():
+    client_id = "12345"
+    redirect_uri = "https://example.com/callback"
+    expected_url = (
+        "https://www.strava.com/oauth/authorize?"
+        f"client_id={client_id}"
+        f"&redirect_uri={redirect_uri}"
+        "&response_type=code"
+        "&scope=read%2Cactivity%3Aread%2Cactivity%3Awrite"
+    )
+    assert build_auth_url(client_id, redirect_uri) == expected_url
